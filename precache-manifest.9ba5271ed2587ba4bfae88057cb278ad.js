ee010165b7e470a068449a9c1d405305173d9441
user <1111>
2020-05-25 17:05:59 +0300

    Updates

---
+++ precache-manifest.9ba5271ed2587ba4bfae88057cb278ad.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af312471bdb8c79fae92c4f0b3e79ce",
+    "url": "/it-kamasutra-95/index.html"
+  },
+  {
+    "revision": "3ca430fc6bf34597ff19",
+    "url": "/it-kamasutra-95/static/css/4.f3439df6.chunk.css"
+  },
+  {
+    "revision": "6415d34f21d50a6c7f4d",
+    "url": "/it-kamasutra-95/static/css/5.a3ee9151.chunk.css"
+  },
+  {
+    "revision": "f9b4f88dce0b0b9a90b9",
+    "url": "/it-kamasutra-95/static/css/6.1513ee9f.chunk.css"
+  },
+  {
+    "revision": "708eef8dba496b97e9ca",
+    "url": "/it-kamasutra-95/static/css/7.25d7f137.chunk.css"
+  },
+  {
+    "revision": "6a56c1aa33ab6cb39fbb",
+    "url": "/it-kamasutra-95/static/css/main.4c4c3ae5.chunk.css"
+  },
+  {
+    "revision": "967a3b51837ba7e9a13c",
+    "url": "/it-kamasutra-95/static/js/0.a1c4313f.chunk.js"
+  },
+  {
+    "revision": "946e861d3674b8b1c10e",
+    "url": "/it-kamasutra-95/static/js/3.5dbb0a00.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/it-kamasutra-95/static/js/3.5dbb0a00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ca430fc6bf34597ff19",
+    "url": "/it-kamasutra-95/static/js/4.01f5d52d.chunk.js"
+  },
+  {
+    "revision": "6415d34f21d50a6c7f4d",
+    "url": "/it-kamasutra-95/static/js/5.eb446f75.chunk.js"
+  },
+  {
+    "revision": "f9b4f88dce0b0b9a90b9",
+    "url": "/it-kamasutra-95/static/js/6.ffda5e99.chunk.js"
+  },
+  {
+    "revision": "708eef8dba496b97e9ca",
+    "url": "/it-kamasutra-95/static/js/7.9842980a.chunk.js"
+  },
+  {
+    "revision": "6a56c1aa33ab6cb39fbb",
+    "url": "/it-kamasutra-95/static/js/main.40aa814a.chunk.js"
+  },
+  {
+    "revision": "0f421d9a2911624b5607",
+    "url": "/it-kamasutra-95/static/js/runtime-main.89658b32.js"
+  },
+  {
+    "revision": "c9041f3f144a80786ab805a59db61c89",
+    "url": "/it-kamasutra-95/static/media/profileAvatar.c9041f3f.png"
+  },
+  {
+    "revision": "ee722eb18a62a8bd9601b721c77f2461",
+    "url": "/it-kamasutra-95/static/media/wallpaper.ee722eb1.jpg"
+  }
+]);
